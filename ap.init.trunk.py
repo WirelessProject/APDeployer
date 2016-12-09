f9f9aefdd058c24442b85e297b0f5b71ac2f6e02
pzread <netfirewall@gmail.com>
2016-12-09 15:31:02 +0800

    Done basic deploying function

--- ap.init.trunk.py
+++ ap.init.trunk.py
@@ -1,54 +1,59 @@
 #!/usr/bin/env python3
 
-import pexpect
 from pexpect import pxssh
+import argparse
+import pexpect
 import sys
 
 # For reading configuration file.
 import os
-from dotenv import Dotenv 
-
+from dotenv import Dotenv
 
 
 class ApInit:
-    def __init__ (self):
+    def __init__(self):
         # Loads configuration file.
 
         dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
         os.environ.update(dotenv)
 
-        self.ap_target = os.environ.get("TARGET")
-        self.dns = [os.environ.get("DNS1"), os.environ.get("DNS2")]
-        self.ZD_IP = os.environ.get("ZD_IP") 
-        self.vlan = os.environ.get("VLAN")
-        self.username = os.environ.get("USER")
-        self.passwd = os.environ.get("PASSWORD")
+        self.ap_target = os.environ.get("AP_IP")
+        self.ZD_IP = os.environ.get("ZONEDIRECTOR_IP")
+        self.gateway = os.environ.get("AP_GATEWAY_IP")
+        self.vlan = int(os.environ.get("AP_MGNT_VLAN"))
+        self.dns = os.environ.get("AP_DNS")
+        self.username = os.environ.get("AP_USERNAME")
+        self.passwd = os.environ.get("AP_PASSWORD")
 
-        print("AP target:", self.ap_target)
-        print("DNS:", self.dns)
-        print("Zone director:", self.ZD_IP)
-        print("Vlan:", self.vlan)
+        parser = argparse.ArgumentParser(
+            description="Deploy ZoneFlex AP configurations"
+        )
+        parser.add_argument(
+            "name",
+            type=str,
+            help="Device name"
+        )
+        parser.add_argument(
+            "identifier",
+            type=int,
+            help="Product Number last 3 digits"
+        )
+        args = parser.parse_args()
 
         # Handles product ID and transfer to IP.
-        if (len(sys.argv) != 2) or (len(sys.argv[1]) != 3):
-            print("Usage: ap.set.exp [Product Number last 3 digits]")
-            print("ex: ./$SCRIPT 937")
-            sys.exit(1)
-
-        self.ID = int(sys.argv[1])
+        self.name = args.name
+        self.ID = args.identifier
         self.IDtoIP()
-        print("IP:", self.IP)
+
+        print("AP target:", self.ap_target)
+        print("Zone director:", self.ZD_IP)
+        print("Vlan:", self.vlan)
+        print("Assigned IP:", self.IP)
 
     def run(self):
-        keygen = pexpect.spawn("ssh-keygen -R %s" % self.ap_target)
-        keygen.close
-
-        try:
-            ssh = pexpect.spawn("ssh %s" % self.ap_target)
-            ssh.expect(["established"])
-            ssh.sendline("yes")
-            # Not actually login.
-
+        with pexpect.spawn(
+            "ssh -o StrictHostKeyChecking=no %s" % self.ap_target
+        ) as ssh:
             # Real login here.
             ssh.expect([ "login" ])
             ssh.sendline(self.username)
@@ -58,37 +63,40 @@ class ApInit:
             # Sets up interface.
             ssh.expect([ "rkscli:" ])
             ssh.sendline("set interface eth0 type vlan-trunk untag 1")
-            print(ssh.after)
+            ssh.expect([ "OK" ])
 
             ssh.expect([ "rkscli:" ])
             ssh.sendline("set interface eth1 type vlan-trunk untag 1")
-            print(ssh.after)
-
-            # Sets up DNS, ZD and other informations.
-            ssh.expect([ "rkscli:" ])
-            ssh.sendline([ "set dns %s" % " ".join(self.dns) ])
-            print(ssh.after)
-
-            ssh.expect([ "rkscli:" ])
-            ssh.sendline("set device-location 3140403020%d" % self.ID)
+            ssh.expect([ "OK" ])
 
             ssh.expect([ "rkscli:" ])
             ssh.sendline("set director ip %s" % self.ZD_IP)
-
-            ssh.expect([ "rkscli:" ])
-            ssh.sendline("set ipmode wan ipv4")
+            ssh.expect([ "OK" ])
 
             ssh.expect([ "rkscli:" ])
             ssh.sendline(
-                "set ipaddr wan vlan %d %s 255.255.248.0 10.3.7.254"
-                % (self.vlan, self.IP)
+                "set ipaddr wan vlan %d %s 255.255.248.0 %s"
+                % (self.vlan, self.IP, self.gateway)
             )
+            ssh.expect([ "OK" ])
 
             ssh.expect([ "rkscli:" ])
-        except:
-            print("Exception was thrown")
-            print(str(ssh))
+            ssh.sendline("set ipmode wan ipv4")
+            ssh.expect([ "OK" ])
 
+            ssh.expect([ "rkscli:" ])
+            ssh.sendline("set dns %s" % self.dns)
+            ssh.expect([ "OK" ])
+
+            ssh.expect([ "rkscli:" ])
+            ssh.sendline("set director ip %s" % self.ZD_IP)
+            ssh.expect([ "OK" ])
+
+            ssh.expect([ "rkscli:" ])
+            ssh.sendline("set device-name %s" % self.name)
+            ssh.expect([ "OK" ])
+
+            pexpect.spawn("ssh-keygen -R %s" % self.ap_target).close()
 
     def IDtoIP(self):
         div = 8 + self.ID // 256
